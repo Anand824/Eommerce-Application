db9e1b8be1d11af656f76032cecd5413537794dc
anndprakash <anndprakash@gmail.com>
2024-02-28 15:24:43 -0500

    modified

--- Ecommer-Application1/src/main/java/com/commerce/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.commerce.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
